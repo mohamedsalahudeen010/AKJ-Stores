5e95c6514c73d998d9cdffacc60f8158bbc63620
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-11-21 03:42:15 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -6,9 +6,14 @@ export const AkjContext=createContext(null)
 function Context(props) {
     const [openCom, setOpenCom] = useState(false);
     const [openLand, setOpenLand] = useState(false);
+    const [mobile,setMobile]=useState(false)
+    const [electronics,setElectronics]=useState(false)
+    const [toys,setToys]=useState(false)
+    const [furniture,setFurniture]=useState(false)
     const baseURL="https://shine-traders-back-end.vercel.app"
   return (
-    <AkjContext.Provider value={{openCom,setOpenCom,openLand,setOpenLand,baseURL}}>
+    <AkjContext.Provider value={{openCom,setOpenCom,openLand,setOpenLand,baseURL,mobile,setMobile,electronics,setElectronics
+    ,toys,setToys,furniture,setFurniture}}>
         {props.children}
     </AkjContext.Provider>
   )
