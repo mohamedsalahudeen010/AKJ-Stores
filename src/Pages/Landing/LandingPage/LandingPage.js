8a5135d8307187a04715a89c2b0b63daf65b0ce8
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-11-21 13:54:01 +0530

    Update LandingPage.js

--- src/Pages/Landing/LandingPage/LandingPage.js
+++ src/Pages/Landing/LandingPage/LandingPage.js
@@ -17,33 +17,7 @@ function LandingPage() {
   
     </div>
 
-    <div className='row'>
-      <div className='col'>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/AX7-BGXFTCg" title="YouTube video player" frameBorder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
-
-      <div className='col'>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/lZMpbWLcLAY" title="YouTube video player" frameBorder="0"
-       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
-
-      
-    </div>
-
-    <div className='row'>
-      <div className='col'>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/qZotvuziHmw" title="YouTube video player" frameBorder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-   </div>
-
-      <div className='col'>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/FsrHHH6wfbA" title="YouTube video player" frameBorder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
-
-      
-    </div>
+    
 
     
        </div>
