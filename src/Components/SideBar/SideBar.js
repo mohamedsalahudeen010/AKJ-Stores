e778d0de0144d23be3b20a474a839fbc8eb43f3c
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2023-11-23 16:22:23 +0530

    Update SideBar.js

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -4,15 +4,19 @@ import { AkjContext } from "../../Context";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useDispatch } from "react-redux";
+import { fetchProductsWithQuery } from "../../Redux/Products/productsAction";
 
 
 function SideBar() {
     const {sideNavProduct,SetsideNavProduct}=useContext(AkjContext)
+    const dispatch=useDispatch()
   return (
     <div className="sideBar">
        
        <div className="sidebar-title">
-        <img src="https://rukminim1.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100" alt="mobile"/>
+        <img src="https://rukminim1.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100" alt="mobile"
+        onClick={()=>{dispatch(fetchProductsWithQuery("mobile"))}}/>
             <NavDropdown
              
               title="Mobiles"
@@ -20,25 +24,46 @@ function SideBar() {
               variant="dark"
               id="dropdown-custom-components"
             >
-              <NavDropdown.Item>Samsung</NavDropdown.Item>
-              <NavDropdown.Item>iPhone</NavDropdown.Item>
-              <NavDropdown.Item >One Plus</NavDropdown.Item>
-              <NavDropdown.Item>Vivo</NavDropdown.Item>
-              <NavDropdown.Item>Oppo</NavDropdown.Item>
-              <NavDropdown.Item >Poco</NavDropdown.Item>
-              <NavDropdown.Item>Xiaomi</NavDropdown.Item>
-              <NavDropdown.Item>Redmi</NavDropdown.Item>
-              <NavDropdown.Item >Motorola</NavDropdown.Item>
-              <NavDropdown.Item>Realme</NavDropdown.Item>
-              <NavDropdown.Item>Infinix</NavDropdown.Item>
-              <NavDropdown.Item >Techno</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("samsung"))}}
+              >Samsung</NavDropdown.Item>
+
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("iphone"))}}
+               >iPhone</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("one plus"))}}
+               >One Plus</NavDropdown.Item>
+
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("vivo"))}}
+              >Vivo</NavDropdown.Item>
+
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("oppo"))}}
+              >Oppo</NavDropdown.Item>
+              
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("poco"))}}
+              >Poco</NavDropdown.Item>
+              
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("xiaomi"))}}
+              >Xiaomi</NavDropdown.Item>
+
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("redmi"))}}
+              >Redmi</NavDropdown.Item>
+              
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("motorola"))}}
+              >Motorola</NavDropdown.Item>
+
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("realme"))}}
+              >Realme</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("infinix"))}}
+              >Infinix</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("techno"))}}
+              >Techno</NavDropdown.Item>
              
             </NavDropdown>
         
 
        </div>
        <div className="sidebar-title">
-       <img src="https://rukminim1.flixcart.com/fk-p-flap/80/80/image/0d75b34f7d8fbcb3.png?q=100" alt="fashion"/>
+       <img src="https://rukminim1.flixcart.com/fk-p-flap/80/80/image/0d75b34f7d8fbcb3.png?q=100" alt="fashion"
+        onClick={()=>{dispatch(fetchProductsWithQuery("fashion"))}}/>
             <NavDropdown
              
               title="Fashion & lifeStyle"
@@ -46,23 +71,25 @@ function SideBar() {
               variant="dark"
               id="dropdown-custom-components"
             >
-              <NavDropdown.Item>Men Pants</NavDropdown.Item>
-              <NavDropdown.Item>Men Jeans</NavDropdown.Item>
-              <NavDropdown.Item >Shirt</NavDropdown.Item>
-              <NavDropdown.Item>Trousers</NavDropdown.Item>
-              <NavDropdown.Item>Track suit</NavDropdown.Item>
-              <NavDropdown.Item >T-Shirt</NavDropdown.Item>
-              <NavDropdown.Item>Women Jeans</NavDropdown.Item>
-              <NavDropdown.Item>Chudithars</NavDropdown.Item>
-              <NavDropdown.Item >Sarees</NavDropdown.Item>
-              <NavDropdown.Item>Gowns</NavDropdown.Item>
-              <NavDropdown.Item>Lehenga Choli</NavDropdown.Item>
-              <NavDropdown.Item >Salwars & Patiala</NavDropdown.Item>
+              <NavDropdown.Item
+               onClick={()=>{dispatch(fetchProductsWithQuery("men pants"))}}>Men Pants</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("men jean"))}}>Men Jeans</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("shirt"))}}>Shirt</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("trousers"))}}>Trousers</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("track suit"))}}>Track suit</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("t shirt"))}}>T-Shirt</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("women jeans"))}}>Women Jeans</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("Chudithars"))}}>Chudithars</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("sarees"))}}>Sarees</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("gowns"))}}>Gowns</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("lehanga"))}}>Lehenga</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("Salwars"))}}>Salwars & Patiala</NavDropdown.Item>
             </NavDropdown>
             </div>
 
        <div className="sidebar-title">
-       <img src="https://rukminim1.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100" alt="electronics"/>
+       <img src="https://rukminim1.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100" alt="electronics"
+       onClick={()=>{dispatch(fetchProductsWithQuery("electronics"))}}/>
             <NavDropdown
              
               title="Electronics"
@@ -70,23 +97,25 @@ function SideBar() {
               variant="dark"
               id="dropdown-custom-components"
             >
-             <NavDropdown.Item>Laptop</NavDropdown.Item>
-              <NavDropdown.Item>Tv</NavDropdown.Item>
-              <NavDropdown.Item >HeadPhones & heatsets</NavDropdown.Item>
-              <NavDropdown.Item>Airpod (TWS)</NavDropdown.Item>
-              <NavDropdown.Item>Mobile Charger</NavDropdown.Item>
-              <NavDropdown.Item >SoundBar</NavDropdown.Item>
-              <NavDropdown.Item>Monitor</NavDropdown.Item>
-              <NavDropdown.Item>Hard Disk</NavDropdown.Item>
-              <NavDropdown.Item >Pendrive</NavDropdown.Item>
-              <NavDropdown.Item>Mouse</NavDropdown.Item>
-              <NavDropdown.Item>Bluetooth Speakers</NavDropdown.Item>
-              <NavDropdown.Item >Keyboard</NavDropdown.Item>
+             <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("laptop"))}}>Laptop</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("computer"))}}>Computer</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("headPhones & headsets"))}} >HeadPhones & headsets</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("airpod"))}}>Airpod (TWS)</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("mobile charger"))}}>Mobile Charger</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("soundbar"))}}>SoundBar</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("monitor"))}}>Monitor</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("hard disk"))}}>Hard Disk</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("pendrive"))}}>Pendrive</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("mouse"))}}>Mouse</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("bluetooth speakers"))}}>Bluetooth Speakers</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("keyboard"))}}>Keyboard</NavDropdown.Item>
             </NavDropdown>
        </div>
 
        <div className="sidebar-title">
-       <img src="https://rukminim1.flixcart.com/flap/80/80/image/0ff199d1bd27eb98.png?q=100" alt="appliances"/>
+       <img src="https://rukminim1.flixcart.com/flap/80/80/image/0ff199d1bd27eb98.png?q=100" alt="appliances"
+       onClick={()=>{dispatch(fetchProductsWithQuery("appliances"))}}
+       />
             <NavDropdown
              
               title="Appliances"
@@ -94,23 +123,24 @@ function SideBar() {
               variant="dark"
               id="dropdown-custom-components"
             >
-             <NavDropdown.Item>Tv</NavDropdown.Item>
-              <NavDropdown.Item>Washing Machine</NavDropdown.Item>
-              <NavDropdown.Item >Refridgrerator</NavDropdown.Item>
-              <NavDropdown.Item>Air Conditioner</NavDropdown.Item>
-              <NavDropdown.Item>Water Heater</NavDropdown.Item>
-              <NavDropdown.Item >Microwave Owen</NavDropdown.Item>
-              <NavDropdown.Item>Irons</NavDropdown.Item>
-              <NavDropdown.Item>Water Purifier</NavDropdown.Item>
-              <NavDropdown.Item >Mixie</NavDropdown.Item>
-              <NavDropdown.Item>Grinder</NavDropdown.Item>
-              <NavDropdown.Item>Fan</NavDropdown.Item>
-              <NavDropdown.Item >Air Cooler</NavDropdown.Item>
+             <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("tv"))}}>Tv</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("washing machine"))}}>Washing Machine</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("refrigerator"))}}>Refridgrerator</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("air conditioner"))}}>Air Conditioner</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("water heater"))}}>Water Heater</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("microwave owen"))}}>Microwave Owen</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("irons"))}}>Irons</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("water purifier"))}}>Water Purifier</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("mixie"))}}>Mixie</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("grinder"))}}>Grinder</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("fan"))}}>Fan</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("air cooler"))}} >Air Cooler</NavDropdown.Item>
             </NavDropdown>
 
        </div>
        <div className="sidebar-title">
-       <img src="https://rukminim1.flixcart.com/flap/80/80/image/ab7e2b022a4587dd.jpg?q=100" alt="furniture"/>
+       <img src="https://rukminim1.flixcart.com/flap/80/80/image/ab7e2b022a4587dd.jpg?q=100" alt="furniture"
+       onClick={()=>{dispatch(fetchProductsWithQuery("furniture"))}}/>
             <NavDropdown
              
               title="Home & Furniture"
@@ -118,23 +148,24 @@ function SideBar() {
               variant="dark"
               id="dropdown-custom-components"
             >
-              <NavDropdown.Item>3 Seater Sofa</NavDropdown.Item>
-              <NavDropdown.Item>5 Seater Sofa</NavDropdown.Item>
-              <NavDropdown.Item >Corner Sofa</NavDropdown.Item>
-              <NavDropdown.Item>Bed</NavDropdown.Item>
-              <NavDropdown.Item>Mattress</NavDropdown.Item>
-              <NavDropdown.Item >Dining Table</NavDropdown.Item>
-              <NavDropdown.Item>Wooden Cot</NavDropdown.Item>
-              <NavDropdown.Item>BedSheets</NavDropdown.Item>
-              <NavDropdown.Item >Pillows</NavDropdown.Item>
-              <NavDropdown.Item>Coffe Table</NavDropdown.Item>
-              <NavDropdown.Item>Blanket</NavDropdown.Item>
-              <NavDropdown.Item >Clock</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("3 seater sofa"))}}>3 Seater Sofa</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("5 seater sofa"))}}>5 Seater Sofa</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("corner sofa"))}}>Corner Sofa</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("bed"))}}>Bed</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("mattress"))}}>Mattress</NavDropdown.Item>
+              <NavDropdown.Item  onClick={()=>{dispatch(fetchProductsWithQuery("dining table"))}}>Dining Table</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("wooden cot"))}}>Wooden Cot</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("bedsheets"))}}>BedSheets</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("pillows"))}}>Pillows</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("coffee table"))}}>Coffee Table</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("blanket"))}}>Blanket</NavDropdown.Item>
+              <NavDropdown.Item onClick={()=>{dispatch(fetchProductsWithQuery("clock"))}}>Clock</NavDropdown.Item>
             </NavDropdown>
        </div>
 
        <div className="sidebar-title">
-       <img src="https://rukminim1.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100" alt="grocery"/>
+       <img src="https://rukminim1.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100" alt="grocery"
+       onClick={()=>{dispatch(fetchProductsWithQuery("grocery"))}}/>
             <NavDropdown
              
               title="Grocery"
